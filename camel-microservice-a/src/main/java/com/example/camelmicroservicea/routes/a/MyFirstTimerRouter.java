60841429e605204ef54a0184fd4ee8174b86d3c3
anushkadeshpande <anushkaa5000@gmail.com>
2023-08-23 21:29:39 +0530

    transformation and processing demonstration

--- camel-microservice-a/src/main/java/com/example/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/example/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,10 +1,21 @@
 package com.example.camelmicroservicea.routes.a;
 
+import java.time.LocalDateTime;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder{
+	
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
+	
+	@Autowired
+	private SimpleLoggingProcessingComponent simpleLoggingComponent;
 
 	@Override
 	public void configure() throws Exception {
@@ -12,7 +23,14 @@ public class MyFirstTimerRouter extends RouteBuilder{
 		
 		// timer
 		from("timer:first-timer")
+		.log("${body}")
 		.transform().constant("My constant message")
+		.log("${body}")
+//		.transform().constant("Time now is " + LocalDateTime.now())
+		.bean("getCurrentTimeBean", "getCurrentTime")	// transformation
+		.log("${body}")
+		.bean(simpleLoggingComponent)	// processing
+		.log("${body}")
 		.to("log:first-timer");
 		// transformation
 		
@@ -20,4 +38,21 @@ public class MyFirstTimerRouter extends RouteBuilder{
 		
 	}
 
+}
+
+
+@Component
+class GetCurrentTimeBean {
+	public String getCurrentTime() {
+		return "Time now is" + LocalDateTime.now();
+	}
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+	
+	public void process(String message) {
+		logger.info("SimpleLoggingProcessingComponent {}", message);
+	}
 }
